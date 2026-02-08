f4ea89f9b5e86e97805820e830c7c5ed6c639b63
timothy-2183 <timothyhansh@gmail.com>
2026-02-07 17:09:33 -0800

    removed unused comment

--- code/sender_stop_and_wait.py
+++ code/sender_stop_and_wait.py
@@ -21,9 +21,6 @@ def send():
             if not chunk:
                 break
             # as of right noe we have the individual chunks of the data in 1024 bytes, now we can send
-            # logic will be a modified version of this 
-            # https://stackoverflow.com/questions/5343358/stop-and-wait-socket-programming-with-udp#:~:text=The%20stop%20and%20wait%20protocol,before%20sending%20the%20next%20packet.
-            # the link is being done in C, but we need to prepend sequence number before sending and then wait for timeouts if there isn't a proper ack
             packet = sequence_id_size.to_bytes(sequence_id_size, 'big') + chunk
             ackget =  False
             startppd = time.time()
