a8e4a3695fe997e6148ee2e326e0e81a25bc56ee
timothy-2183 <timothyhansh@gmail.com>
2026-02-07 16:05:16 -0800

    Finished initial for stop and wait, have not done checking or debugging

--- code/sender_stop_and_wait.py
+++ code/sender_stop_and_wait.py
@@ -1,18 +1,56 @@
 import socket
 import time
-# create socket outside because we can always reuse
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+# create socket outside because we can always reuse, also set timeout.
+
 receiverip = "127.0.0.1"
 receiverport = "5001"
+packet_sz = 1024 # we read 1024 bytes
+sequence_id_size = 4
+tp = []
+ppd = []
 
-def main():
+def send():
+    starttp = time.time()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(1)
     with open('docker\file.mp3', 'read') as f:
+        # we always start from packet 0
+        seqnum = 0
         while True:
-            chunk = f.read(1024) # we read 1024 bytes
+            chunk = f.read(packet_sz) 
             if not chunk:
                 break
             # as of right noe we have the individual chunks of the data in 1024 bytes, now we can send
             # logic will be a modified version of this 
             # https://stackoverflow.com/questions/5343358/stop-and-wait-socket-programming-with-udp#:~:text=The%20stop%20and%20wait%20protocol,before%20sending%20the%20next%20packet.
-            # the link is being done in C, but we're doing python now
-            sock.sendto(chunk, ())
+            # the link is being done in C, but we need to prepend sequence number before sending and then wait for timeouts if there isn't a proper ack
+            packet = sequence_id_size.to_bytes(sequence_id_size, 'big') + chunk
+            ackget =  False
+            startppd = time.time()
+            while not ackget:
+                #send here regardless of outcome
+                sock.sendto(packet,(receiverip, receiverport))
+                try:
+                    #wait until u receive something
+                    ackpacket, _ = sock.recvfrom(packet_sz)
+                    acknum = int.from_bytes(ackpacket[:sequence_id_size], 'big')
+                    #if correct, u just add smth more if not then just do nothing because the while will just retransmit
+                    if acknum == (seqnum + len(chunk)):
+                        ackget = True
+                        seqnum += len(chunk)
+                        endppd = time.time()
+                        ppd.append(endppd-startppd)
+                    
+                except socket.timeout:
+                    pass
+    sock.close()
+    endtp= time.time()
+    tp.append(endtp-starttp)
+def main():
+    for _ in range(10):
+        send
+    avgtp = sum(tp)/len(tp)
+    avgppd = sum(ppd)/len(ppd)
+    print(avgtp)
+    print(avgppd)
+    print(0.3*avgtp/1000 + 0.7/avgppd)
