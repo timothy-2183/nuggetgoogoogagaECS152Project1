f66d9efc1b7d477e19c0da3c3f9deb37ccde681e
Dharnish Bose Ramesh <dboseramesh@ucdavis.edu>
2026-02-11 13:57:00 -0800

    empty commit

--- docker/sender_fixed_swp2.py
+++ docker/sender_fixed_swp2.py
@@ -45,21 +45,23 @@ class UDPSender:
                 received_ack_packet, _ = self.Socket.recvfrom((SEQ_ID_SIZE + EXTRA_BUFFER_SPACE))
                 received_ack_id = int.from_bytes(received_ack_packet[:SEQ_ID_SIZE],signed=True, byteorder='big')
                 while (received_ack_id >= expected_ack_id):
+                    if not self.SlidingWindow:
+                        break
                     #pop self.DictOfPackets
                     packet_seq_id = expected_ack_id - MESSAGE_SIZE
                     if packet_seq_id in self.DictOfPackets:
                         self.DictOfPackets.pop(packet_seq_id)
                         print("Packet", expected_ack_id, "acknowledged") #DEBUG
-                    # send the next packet right outside sliding window
+                    # send the next packet right outside sliding window, and then slide it
                     next_seq_id = self.SlidingWindow[-1] + MESSAGE_SIZE
                     if next_seq_id in self.DictOfPackets:
                         packet = self.DictOfPackets[next_seq_id]
                         self.Socket.sendto(packet, self.address)
                         # slide the sliding window
-                        self.SlidingWindow.remove(packet_seq_id)
                         self.SlidingWindow.append(next_seq_id)
+                    self.SlidingWindow.remove(packet_seq_id)
                         # increment expected_ack_id
-                        expected_ack_id += MESSAGE_SIZE
+                    expected_ack_id += MESSAGE_SIZE
             except socket.timeout:
                 print("TIMEOUT! RETRANSMITING")
                 for seq_id in self.SlidingWindow:
